48b21026d9ff43d41bb4c46146152a9fb61338fe
Najmeh Zare <81696401+najmehzare@users.noreply.github.com>
2024-06-04 08:59:13 +0330

    fix:bug

--- src/layout/main.tsx
+++ src/layout/main.tsx
@@ -3,58 +3,57 @@ import LinearProgress, { LinearProgressProps } from '@mui/material/LinearProgres
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
 import SankeyChart from "@/components/sankey-chart";
 import EnhancedTable from "@/components/table";
+import Stack from '@mui/material/Stack';
 
 function LinearProgressWithLabel(props: LinearProgressProps & { value: number }) {
-  return (
-    <Box sx={{ display: 'flex', alignItems: 'center' }}>
-      <Box sx={{ width: '100%', mr: 1 }}>
-        <LinearProgress variant="determinate" {...props} />
-      </Box>
-      <Box sx={{ minWidth: 35 }}>
-        <Typography variant="body2" color="text.secondary">{`${Math.round(
-          props.value,
-        )}%`}</Typography>
-      </Box>
-    </Box>
-  );
+    return (
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Box sx={{ width: '100%', mr: 1 }}>
+                <LinearProgress variant="determinate" {...props} />
+            </Box>
+            <Box sx={{ minWidth: 35 }}>
+                <Typography variant="body2" color="text.secondary">{`${Math.round(
+                    props.value,
+                )}%`}</Typography>
+            </Box>
+        </Box>
+    );
 }
 
 export default function Main() {
 
-  const [progress, setProgress] = React.useState(10);
+    const [progress, setProgress] = React.useState(10);
 
-  React.useEffect(() => {
-    const timer = setInterval(() => {
-      setProgress((prevProgress) => (prevProgress + 20));
-    }, 800);
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
+    React.useEffect(() => {
+        const timer = setInterval(() => {
+            setProgress((prevProgress) => (prevProgress + 30));
+        }, 800);
+        return () => {
+            clearInterval(timer);
+        };
+    }, []);
 
-  return (
-    <div className="p-5 m-5">
-      <Card sx={{ padding: '0px' }}>
-        {
-          progress <= 100 ?
-            <div className='p-10 flex justify-center'>
-              <Box sx={{ width: '80%', direction: 'ltr' }}>
-                <LinearProgressWithLabel value={progress} />
-              </Box>
-            </div>
-            :
-            <SankeyChart />
-        }
-
-        <CardContent>
-          <div dir="rtl" className="pt-10">
-            <EnhancedTable />
-          </div>
-        </CardContent>
-      </Card>
-    </div>
-  );
+    return (
+        <div>
+            <Stack sx={{ padding: '10px', margin: '15px' }}>
+                <Card>
+                    <div className='p-10 flex justify-center'>
+                        {
+                            progress <= 100 ?
+                                <Box sx={{ width: '80%', direction: 'ltr' }}>
+                                    <LinearProgressWithLabel value={progress} />
+                                </Box>
+                                :
+                                <SankeyChart />
+                        }
+                    </div>
+                </Card>
+            </Stack>
+            <Stack sx={{ padding: '10px', margin: '15px' }} >
+                <EnhancedTable />
+            </Stack>
+        </div>
+    );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,20 @@
 import MenuAppBar from "@/layout/header";
 import Main from "@/layout/main";
+import MainSidebar from "@/layout/sidebar";
 
 export default function Home() {
 
   return (
     <div>
       <MenuAppBar />
-      <Main />
+      <div className='flex flex-row justify-start'>
+        <div>
+          <MainSidebar />
+        </div>
+        <div className='flex flex-col justify-center bg-slate-100 p-2 w-full'>
+          <Main />
+        </div>
+      </div>
     </div>
   );
 }
